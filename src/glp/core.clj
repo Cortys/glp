658dd91e3c32389704543eddcaf927b6f5c4e52e
Clemens Damke <admin@cortys.de>
2018-03-07 13:21:41 +0100

    sorting experiments

--- src/glp/core.clj
+++ src/glp/core.clj
@@ -1,6 +1,32 @@
 (ns glp.core
-  (:require [clojure.core.logic :refer :all])
+  (:require [clojure.core.logic :refer :all]
+            [clojure.core.logic.fd :as fd]
+            [clojure.core.logic.pldb :as db])
   (:refer-clojure :exclude [==]))
 
+(def x 1)
+(def l [1 2 3 1])
+
 (run* [q]
-  (== q true))
+  (conde
+    [(firsto l x) (resto l q)]
+    [(fresh [y] (firsto l y) (!= x y) (== l q))]))
+
+(run* [q]
+  (!= q 1) (!= q 2))
+
+(defn orderedo [l]
+  (matche [l]
+    ([[f . r]]
+     (fresh [s]
+       (firsto r s)
+       (fd/<= f s)
+       (orderedo r)))
+    ([[_]] succeed)
+    ([[]] succeed)))
+
+(let [l (repeatedly 7 #(rand-int 100))]
+  (println l)
+  (time (first (run 1 [q]
+                 (permuteo l q)
+                 (orderedo q)))))
